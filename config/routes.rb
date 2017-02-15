31e82ad002ecb3444c4cfa99a672ad5fca438607
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2017-02-15 12:08:14 -0200

    cara pagou

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :apolices
-  #root 'apolices#index'
-  root 'apolices#home'
+  root 'apolices#index'
+  #root 'apolices#home'
   get 'apolices/relatorio'
   get "/relatorio", to: "apolices#relatorio", as: :relatorio
 
