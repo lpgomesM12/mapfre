fa035e6a21c29b73d8bb6aecf30b3a18c8d91d58
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2019-01-22 10:17:45 -0200

    correcao mapre

--- app/models/apolice.rb
+++ app/models/apolice.rb
@@ -1,6 +1,7 @@
 class Apolice < ActiveRecord::Base
 
-
+ has_many :coberturaapolices, dependent: :destroy
+ has_many :servicos, dependent: :destroy
 
  def self.search(term, condominio, apartamento_id = 0)
       sql = sql + "lower(pe.cpf) like '%#{term.downcase}%' AND "
